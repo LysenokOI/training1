3e5c214dc2ccd69d2eabb59f27eef1886edd2aa2
Oleg Lysenok <msk.lysenok@gmail.com>
2018-10-28 23:08:06 +0300

    added steps for splitting

--- start.py
+++ start.py
@@ -1,2 +1,16 @@
-a = "pasha_devman"
-print(a)
+import chardet
+
+def define_file_encoding(filepath):
+    with open(filepath, 'rb') as raw_content:
+        return chardet.detect(raw_content.read()).get('encoding')
+
+def load_raw_content(filepath, encoding):
+    with open(filepath, mode='r', encoding=encoding) as raw_content:
+        return raw_content.read()
+
+
+define_file_encoding('Ferris.txt')
+load_raw_content('Ferris.txt', 'encoding')
+
+for line in raw_content:
+    print(line)
