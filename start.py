e69c5bf3c9e5cc7c09233c3c9106159a0c754a2c
Oleg Lysenok <msk.lysenok@gmail.com>
2018-10-30 22:10:10 +0300

    PASHA GOTOVOOOOOOOOOOOOOOOOOOO. CYU TEKCT

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 import chardet
 import re
-import operator
 
 def define_file_encoding(filepath):
     with open(filepath, 'rb') as raw_content:
@@ -14,18 +13,18 @@ def word_catalg(lst):
     catalg = {}
     for itm in lst:
         if itm not in catalg:
-            catalg[itm] = 1  # create couple with value 1. Я знаю, что здесь itm неправильно
+            catalg[itm] = 1
         else:
-            catalg[itm] += 1  # и здесь
+            catalg[itm] += 1
     return(catalg)
 
 
 def output_max(max_frq_word, word_frq_cat):
     for value in max_frq_word:
         if len(max_frq_word) > 1:
-            print(value, word_frq_cat[value])
+            print('Most common word', max_frq_word.index(value) + 1, ': ', value, ', Found in the text ', word_frq_cat[value], ' times')
         else:
-            print(value, word_frq_cat[value])
+            print('Most common word:', value, ', Found in the text ', word_frq_cat[value], ' times')
 
 
 print('Input file name:')
@@ -34,7 +33,7 @@ encod_file = define_file_encoding(text_file)
 read_text = load_raw_content(text_file, encod_file)
 reg_ex = re.compile("[\w']+")
 list_text = reg_ex.findall(read_text)
-# print(list_text)  удалить позже
+# print(list_text)  если хочется посмотреть на список
 word_freq_catalog = word_catalg(list_text)  # function for creating couples
 max_freq_word = [key for key, val in word_freq_catalog.items() if val == max(word_freq_catalog.values())] # key
 output_max(max_freq_word, word_freq_catalog)
