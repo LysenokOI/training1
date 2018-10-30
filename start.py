982c74a73525513f327524df3cabdf0f931534f1
Oleg Lysenok <msk.lysenok@gmail.com>
2018-10-30 20:06:57 +0300

    still trying to create function for catalog

--- start.py
+++ start.py
@@ -9,13 +9,14 @@ def load_raw_content(filepath, encoding):
     with open(filepath, mode='r', encoding=encoding) as raw_content:
         return raw_content.read()
 
-def word_catlg(lst, catalg):
-    for itm in list:
+def word_catalg(lst):
+    catalg = {}
+    for itm in lst:
         if itm not in catalg:
-            new_catalg = dict.fromkeys([lst[itm]=1)  # create couple with value 1
+            catalg = dict(itm=1)  # create couple with value 1. Я знаю, что здесь itm неправильно
         else:
-            # add couple
-            catalg[lst[itm]] = +1
+            catalg[itm] = +1  # и здесь
+        return(catalg)
 
 
 print('Input file name:')
@@ -24,5 +25,6 @@ encod_file = define_file_encoding(text_file)
 read_text = load_raw_content(text_file, encod_file)
 reg_ex = re.compile("[\w']+")
 list_text = reg_ex.findall(read_text)
-print(list_text)  # удалить позже
-word_freq = {}
+# print(list_text)  удалить позже
+word_freq = word_catalg(list_text)  # function for creating couples
+print(word_freq)
